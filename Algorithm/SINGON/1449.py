11b6af4ef5da85f1fc2c0bf89be4c6a61fcd96ad
SinGonKim <87477828+SinGonKim@users.noreply.github.com>
2021-09-17 00:41:27 +0900

    Update 1449.py
    docs: 저번에 상민님이 의견 주셨던 것대로 채점 돌려보네 잘 못된게 있었네요. 수정해서 자료로 남깁니다.

--- Algorithm/SINGON/1449.py
+++ Algorithm/SINGON/1449.py
@@ -1,11 +1,14 @@
+import math
 import sys
-num, length = map(int,sys.stdin.readline().split())
-holes = list(map(int,sys.stdin.readline().split()))
-holes.sort()
-"""물이 새는 곳은 가장 왼쪽에서 정수만큼 떨어진 거리이고 (x-0.5,x+0.5)만큼 테이프를 쓰므로
-겹쳐지지 않으며 각 구멍을 막는데 필요한 길이는 1이다.
-따라서 필요한 테이프 개수는 구멍개수를 테이프 한 개당 길이로 나눈 수에서 소수 첫째자리에서 올림을 하면 된다."""
-if num%length ==0:
-    print(num/length)
-else:
-    print(int(num//length)+1)
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+def choice(N, M):
+    return factorial(M)//(factorial(N)*factorial(M-N)) # //가 아닌 /를 사용하면 .0 이 나와 틀렸다고 뜸!
+T = int(input())
+for i in range(T):
+    N, M = map(int, sys.stdin.readline().split())
+    print(choice(N,M))
